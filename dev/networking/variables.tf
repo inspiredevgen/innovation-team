29e7a3e109069a2d7e0ddf94812c7d6e985a513a
Adi Bal-Mayel <abalmayel@gmail.com>
2025-04-19 10:56:26 -0400

    Deploying Subnets

--- dev/networking/variables.tf
+++ dev/networking/variables.tf
@@ -6,6 +6,13 @@ variable "vpc_cidr_block" {
   default = "10.100.0.0/16"
 }
 
+variable "public_subnet_cidr" {
+  default = "10.100.10.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.100.30.0/24"
+}
 variable "env" {
   default = "dev"
 }
