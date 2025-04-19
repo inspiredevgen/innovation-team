7acc40f47e9f5feca978089a36666df535fd976b
Adi Bal-Mayel <abalmayel@gmail.com>
2025-04-19 10:45:12 -0400

    Deploying VPC in AWS Central

---
+++ dev/networking/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "ca-central-1"
+}
+
+variable "vpc_cidr_block" {
+  default = "10.100.0.0/16"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "team" {
+  default = "BANK123"
+}
+
+variable "project" {
+  default = "WebAPP"
+}
