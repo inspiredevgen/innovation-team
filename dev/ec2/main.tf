7f217cc7d15e804d0459e0b4cb9e7c15825a0208
Adi Bal-Mayel <abalmayel@gmail.com>
2025-04-22 22:20:58 -0400

    Finalizing EZ2 instance

--- dev/ec2/main.tf
+++ dev/ec2/main.tf
@@ -1,8 +1,35 @@
-resource "aws_instance" "website" {
-  ami = var.ami_amazon_linux-2
-  instance_type = var.website_instance_type
-  
-  subnet_id     = data.terraform_remote_state.networking.outputs.public_subnet_id
-  vpc_security_group_ids = [data.terraform_remote_state.networking.outputs.sg_ec2_id] 
+data "terraform_remote_state" "networking" {
+  backend = "remote"
 
+  config = {
+    organization = "InspireDevGen"
+    workspaces = {
+      name = "innovation-team"
+    }
+  }
+}
+
+
+data "aws_subnet" "public_subnet_id" {
+  tags = {
+    Name = "Public Subnet ID"
+  }
+}
+
+data "aws_security_group" "sg_ec2_id" {
+  tags = {
+    Name = "VPC ID"
+  }
+}
+
+
+
+resource "aws_instance" "website" {
+  ami           = var.ami_amazon_linux-2
+  instance_type = var.website_instance_type
+
+  #subnet_id     = data.terraform_remote_state.networking.outputs.public_subnet_id
+  #vpc_security_group_ids = [data.terraform_remote_state.networking.outputs.sg_ec2_id] 
+  subnet_id              = public_subnet_id
+  vpc_security_group_ids = sg_ec2_id
 }
