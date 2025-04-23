7a81e0eccd2812e45fe02e749ca8b08a1b3fbed4
Adi Bal-Mayel <abalmayel@gmail.com>
2025-04-23 19:13:36 -0400

    Deploying IGW 3rd attempt

--- dev/networking/internet-gw.tf
+++ dev/networking/internet-gw.tf
@@ -6,7 +6,3 @@ resource "aws_internet_gateway" "internet_gw" {
   }
 }
 
-resource "aws_internet_gateway_attachment" "gw_attach" {
-  internet_gateway_id = aws_internet_gateway.internet_gw.id
-  vpc_id              = aws_vpc.main-vpc.id
-}
